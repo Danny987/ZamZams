05f9d3ec78c28c316d243db44c3dd21b3a9589ce
Danny987 <crazydrums987@gmail.com>
2014-02-12 17:26:30 -0700

    Now contains an x and y

--- Tile.java
+++ Tile.java
@@ -4,25 +4,38 @@ public class Tile {
 	private char objType = '.';
 	private int width = 1;
 	private int height = 1;
-	
-	public Tile(){
+	int x = 0;
+	int y = 0;
 
-	}
-	
-	public void setTile(char objType, int width, int height){
+	public Tile(char objType, int width, int height, int x, int y){
 		this.objType = objType;
 		this.width = width;
 		this.height = height;
+		this.x = x;
+		this.y = y;
 	}
-	
+
+	public void setTile(char objType, int width, int height, int x, int y){
+		this.objType = objType;
+		this.width = width;
+		this.height = height;
+		this.x = x;
+		this.y = y;
+	}
+
 	public char getChar(){
 		return this.objType;
 	}
-	
+
 	public Tile cloneTile(){
-		Tile clonedTile = new Tile();
-		clonedTile.setTile(this.objType, this.width, this.height);
+		Tile clonedTile = new Tile(this.objType, this.width, this.height, this.x, this.y);
 		return clonedTile;
 	}
+	
+	/*public Tile cloneTile(){
+		Tile clonedTile = new Tile();
+		clonedTile.setTile(this.objType, this.width, this.height, this.x, this.y);
+		return clonedTile;
+	}*/
 
 }
