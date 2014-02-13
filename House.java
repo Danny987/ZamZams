f7f831f890b30d52669c078825f3a584cd852aa0
Danny987 <crazydrums987@gmail.com>
2014-02-12 17:26:59 -0700

    Now contains a list of objects for collision

--- House.java
+++ House.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class House {
 	//HOUSE LAYOUT
-	private char houseLayout[][] = new char[100][100];
 	private Tile[][] tileLayout = new Tile[100][100];
 
 	//HOUSE PROPERTIES
@@ -12,9 +11,15 @@ public class House {
 	private int houseWidth = 0;
 	private int houseLength = 0;
 	private String exitWall = "";
+	
+	//minimum distance from the exit
+	private int exitDistance = 10;
 
 	//list of zombie objects
 	ArrayList<Zombie> zombieList = new ArrayList<>();
+	
+	//list of tile objects
+	ArrayList<Tile> tileList = new ArrayList<>();
 	
 	Random rand = new Random();
 	
@@ -22,7 +27,7 @@ public class House {
 		for (int y=0;y<100;y++)
 		{
 			for (int x=0;x<100;x++)
-		    tileLayout[x][y] = new Tile();  // create each actual Person
+		    tileLayout[x][y] = new Tile('.',1,1,x,y);  // create each actual tile
 		}
 	}
 
@@ -37,14 +42,10 @@ public class House {
 		for (int y = 0; y < houseLength; y++)
 			for (int x = 0; x < houseWidth; x++)
 				if((x == 0) || (x == houseWidth-1) || (y == 0) || (y == houseLength-1)){
-					houseLayout[x][y] = 'W';
-					//tileLayout[x][y] = new Tile('W', 1, 1);
-					tileLayout[x][y].setTile('W', 1, 1);
+					tileLayout[x][y].setTile('X', 1, 1,x,y);
 				} 
 				else{
-					houseLayout[x][y] = '.';
-					//tileLayout[x][y] = new Tile('.', 1, 1);
-					tileLayout[x][y].setTile('.', 1, 1);
+					tileLayout[x][y].setTile('.', 1, 1,x,y);
 				}
 	}
 
@@ -87,12 +88,22 @@ public class House {
 			{
 				if(x == leftX && y == topY)
 				{
-					if(area <= 1) tileLayout[x][y].setTile('S', width, height);
-					else if(area > 9) tileLayout[x][y].setTile('L', width, height);
-					else tileLayout[x][y].setTile('M', width, height);
+					if(area <= 1)
+					{
+						tileLayout[x][y].setTile('S', width, height,x,y);
+						tileList.add(new Tile('S', width, height,x,y));
+					}
+					else if(area > 9){
+						tileLayout[x][y].setTile('L', width, height,x,y);
+						tileList.add(new Tile('L', width, height,x,y));
+					}
+					else{
+						tileLayout[x][y].setTile('M', width, height,x,y);
+						tileList.add(new Tile('M', width, height,x,y));
+					}
 				}
 				else{
-					tileLayout[x][y].setTile('X', 1, 1);
+					tileLayout[x][y].setTile('X', 1, 1,x,y);
 				}
 			}
 		}
@@ -138,9 +149,14 @@ public class House {
 			}
 			for(int y = low; y < high+1; y++){
 				if(y == low)
-					tileLayout[c1][y].setTile('V',1,length);
+				{
+					tileLayout[c1][y].setTile('V',1,length,c1,y);
+					tileList.add(new Tile('V',1,length,c1,y));
+				}
 				else
-					tileLayout[c1][y].setTile('X',1,1);
+				{
+					tileLayout[c1][y].setTile('X',1,1,c1,y);
+				}
 			}
 
 		}
@@ -158,9 +174,14 @@ public class House {
 			for(int x = low; x < high+1; x++)
 			{
 				if(x == low)
-					tileLayout[x][c1].setTile('H',1,length);
+				{
+					tileLayout[x][c1].setTile('H',1,length, x, c1);
+					tileList.add(new Tile('H',1,length, x, c1));
+				}
 				else
-					tileLayout[x][c1].setTile('X',1,1);
+				{
+					tileLayout[x][c1].setTile('X',1,1,x,c1);
+				}
 			}
 			
 		}
@@ -190,9 +211,14 @@ public class House {
 	public boolean createFirettrap(int x, int y) {
 		// TODO Auto-generated method stub
 		if(tileLayout[x][y].getChar() == '.')
-			tileLayout[x][y].setTile('F',1 ,1);
+		{
+			tileLayout[x][y].setTile('F',1 ,1, x, y);
+			tileList.add(new Tile('F',1 ,1, x, y));
+		}
 		else
+		{
 			return false;
+		}
 		return true;
 	}
 
@@ -219,14 +245,14 @@ public class House {
 				{
 					for (int x = 0; x < houseWidth; x++)
 					{
-						if(houseLayout[x][y] == '.')
+						if(tileLayout[x][y].getChar() == '.')
 						{
-							Point tile = new Point(x,y); //point valid tile in list
-							tileList.add(tile);
+							Point validTile = new Point(x,y); //put valid tile in list
+							tileList.add(validTile);
 						}
 					}
 				}		
-
+				//if no valid tiles
 				if(tileList.size() == 0)
 					return false;
 				
@@ -234,13 +260,103 @@ public class House {
 				int r = rand.nextInt(tileList.size()+1);
 
 				//set zombie in houselayout
-				tileLayout[tileList.get(r).x][tileList.get(r).y].setTile('Z', 1, 1);
+				tileLayout[tileList.get(r).x][tileList.get(r).y].setTile('Z', 1, 1, tileList.get(r).x, tileList.get(r).y);
 				
 				//set position in zombie object
 				zombieList.get(i).setPosition(tileList.get(r));
 			}
 			//if the zombie had an x and y
+			tileLayout[position.x][position.y].setTile('Z', 1, 1, position.x, position.y);
 		}
 		return true;
+	}
+	
+	public boolean setPlayerExit()
+	{
+		//convert to a char array
+		char[][] houseLayout = new char[houseWidth][houseLength];
+		for(int y = 0; y < houseLength; y++)
+		{
+			for(int x  = 0; x < houseLength; x++)
+			{
+				houseLayout[x][y] = tileLayout[x][y].getChar();
+			}
+		}
+		
+		if(exitWall.equals("north"))
+		{
+			for(int x = 1; x < houseWidth-1; x++)
+			{
+				if(houseLayout[x][1] == '.')
+					houseLayout = floodFill(x, 1, x, 0, houseLayout);
+			}
+		}
+		else if(exitWall.equals("south"))
+		{
+			for(int x = 1; x < houseWidth-1; x++)
+			{
+				if(houseLayout[x][houseLength-2] == '.')
+					houseLayout = floodFill(x, houseLength-2, x, houseLength-1, houseLayout);
+			}
+		}
+		else if(exitWall.equals("east"))
+		{
+			for(int y = 1; y < houseLength-1; y++)
+			{
+				if(houseLayout[1][y] == '.')
+					houseLayout = floodFill(1, y, 0, y, houseLayout);
+			}
+		}
+		else if(exitWall.equals("west"))
+		{
+			for(int y = 1; y < houseLength-1; y++)
+			{
+				if(houseLayout[houseWidth-2][y] == '.')
+					houseLayout = floodFill(houseWidth-2, y, houseWidth-1, y, houseLayout);
+			}
+		}
+		
+		//print char array
+		/*for(int y = 0; y < houseLength; y++)
+		{
+			for(int x  = 0; x < houseLength; x++)
+			{
+				System.out.print(houseLayout[x][y]);
+			}
+			System.out.print("\n");
+		}*/
+		
+		return true;
+	}
+	
+	public char[][] floodFill(int x, int y, int exitX, int exitY, char[][] houseLayout)
+	{
+		houseLayout[exitX][exitY] = 'E';
+		if(houseLayout[x][y] == '.' || houseLayout[x][y] == 'F')
+		{
+			houseLayout[x][y] = ' ';
+			floodFill(x+1, y, exitX, exitY, houseLayout);
+			floodFill(x-1, y, exitX, exitY, houseLayout);
+			floodFill(x, y+1, exitX, exitY, houseLayout);
+			floodFill(x, y-1, exitX, exitY, houseLayout);
+		}
+		return houseLayout;
+	}
+	
+	public boolean distanceCheck(int x1, int y1, int x2, int y2)
+	{
+		int d = (int)Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		if(d > exitDistance)
+			return true;
+		else
+			return false;
+	}
+	
+	public void printTileList()
+	{
+		for(int i = 0; i < tileList.size(); i++)
+		{
+			System.out.print(tileList.get(i).getChar());
+		}
 	}
 }
