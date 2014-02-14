1b7dbe9d7a3b6122d997c7b2b43bd5f7c49e7a65
Ramón A. Lovato <cubevato@gmail.com>
2014-02-13 20:05:31 -0700

    Fixed problem in ZombieLevel

--- ZombieLevel.java
+++ ZombieLevel.java
@@ -47,7 +47,7 @@ public class ZombieLevel {
      * @param layout 2-D Tile array representing the level's floor layout.
      */
     public ZombieLevel(House current) {
-        this.house = house;
+        this.house = current;
         int width = current.getHouseWidth();
         int length = current.getHouseLength();
         Tile[][] array = current.getHouseLayout();
