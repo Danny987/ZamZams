b8d4420ebaf0f66d770185d24cc0dc07bb3a2a61
davi0011 <davi0011@yahoo.com>
2014-02-16 01:01:49 -0700

    zombies move up

--- Zombie.java
+++ Zombie.java
@@ -11,8 +11,7 @@ public class Zombie extends Character
   private float probabiltyA, probablityB, smell;
   // 1 = north, 2 = east, 3 = south, 4 = west
   // 5 = northeast, 6 = southeast, 7 = southwest, 8 = northwest`
-  private int direction = 1;
-  private int chosenDirection = 0;
+  private int chosenDirection = 1;
   // for line walk zombies
   private int previousDirection = 0;
 
@@ -20,6 +19,8 @@ public class Zombie extends Character
       float probabilityA, float probabilityB)
   {
     this.position = position;
+    this.position.x *= 50;
+    this.position.y *= 50;
     this.smell = smell;
     this.speed = speed;
     this.walkType = walkType;
@@ -27,8 +28,7 @@ public class Zombie extends Character
     this.probablityB = probabilityB;
     // multiply by 50 because the x and y are in tiles not pixels
     // add 5 to center the box a bit because it isn't 50 by 50
-    this.hitbox = new Rectangle((position.x) * 50 + 5, (position.y) * 50 + 5,
-        40, 40);
+    this.hitbox = new Rectangle((position.x) + 5, (position.y) + 5, 40, 40);
   }
 
   public void setSmell(float adjustedSmell)
@@ -39,25 +39,16 @@ public class Zombie extends Character
 
   private void move(Zombie curZombie)
   {
-    // random walk type
-    if (curZombie.walkType == 0)
-    {
-      curZombie.mover();
-    }
 
-    // line walk type
-    else
-    {
-
-    }
+    curZombie.mover();
 
   }
 
-  private void chooseDirection()
+  public void chooseDirection()
   {
     if (this.walkType == 0)
     {
-      if (probabiltyA <= (Math.random() * 100))
+      if (probabiltyA <= (Math.random() ))
       {
         // chooses a random direction to move in
         chosenDirection = (int) (Math.random() * 8);
@@ -71,8 +62,8 @@ public class Zombie extends Character
     }
     else
     {
-      // line walk typ movement
-      if (probabiltyA <= (Math.random() * 100))
+      // line walk type movement
+      if (probabiltyA <= (Math.random() ))
       {
         // chooses a random direction to move in
         chosenDirection = (int) (Math.random() * 7) + 1;
@@ -90,13 +81,87 @@ public class Zombie extends Character
   {
     for (Zombie curZombie : zombieList)
     {
+      System.out.println("*******" + curZombie.getHitbox().toString());
       curZombie.move(curZombie);
     }
   }
 
   private void mover()
   {
-    if (this.chosenDirection == 0)
+    int leftRight = 0, upDown = 0;
+
+    switch (chosenDirection)
+    {
+    case 0:
+    {
+      leftRight = 0;
+      upDown = 0;
+      break;
+    }
+    case 1:
+    {
+      leftRight = 0;
+      upDown = 1;
+      break;
+    }
+    case 2:
+    {
+      leftRight = 2;
+      upDown = 0;
+      break;
+    }
+    case 3:
+    {
+      leftRight = 0;
+      upDown = 2;
+      break;
+    }
+    case 4:
+    {
+      leftRight = 1;
+      upDown = 0;
+      break;
+    }
+    case 5:
+    {
+      leftRight = 2;
+      upDown = 1;
+      break;
+    }
+    case 6:
+    {
+      leftRight = 2;
+      upDown = 2;
+      break;
+    }
+    case 7:
+    {
+      leftRight = 1;
+      upDown = 2;
+      break;
+    }
+    case 8:
+    {
+      leftRight = 1;
+      upDown = 1;
+      break;
+    }
+    }
+    int collide = collision(cMap, leftRight, upDown);
+
+    if (collide == 1)
+    {
+      /**
+       * zombie hit zombie
+       * */
+    }
+    if (this.trapCollision == true)
+    {
+      /**
+       * player has triggered a trap
+       */
+    }
+    else if (this.chosenDirection == 0)
     {
       previousDirection = direction;
     }
@@ -105,7 +170,7 @@ public class Zombie extends Character
     {
 
       // free movement
-      this.position.x -= Math.round(TILE * speed / FRAMERATE);
+      this.position.x -= (TILE * speed / FRAMERATE);
       this.hitbox = new Rectangle(position.x + 5, position.y + 5, 40, 40);
 
       lastMovedDirection = direction;
@@ -118,7 +183,7 @@ public class Zombie extends Character
     {
 
       // free movement
-      this.position.x += Math.round(TILE * speed / FRAMERATE);
+      this.position.x += (TILE * speed / FRAMERATE);
       this.hitbox = new Rectangle(position.x + 5, position.y + 5, 40, 40);
       previousDirection = direction;
       lastMovedDirection = direction;
@@ -129,7 +194,7 @@ public class Zombie extends Character
     {
 
       // free movement
-      this.position.y -= Math.round(TILE * speed / FRAMERATE);
+      this.position.y -= (TILE * speed / FRAMERATE);
       this.hitbox = new Rectangle(position.x + 5, position.y + 5, 40, 40);
       lastMovedDirection = direction;
       previousDirection = direction;
@@ -140,7 +205,7 @@ public class Zombie extends Character
     {
 
       // free movement
-      this.position.y += Math.round(TILE * speed / FRAMERATE);
+      this.position.y += (TILE * speed / FRAMERATE);
       this.hitbox = new Rectangle(position.x + 5, position.y + 5, 40, 40);
       lastMovedDirection = direction;
       previousDirection = direction;
@@ -150,8 +215,8 @@ public class Zombie extends Character
     else if (this.chosenDirection == 8)
     {
       // free movement
-      this.position.x -= Math.round((TILE * speed / FRAMERATE) / Math.sqrt(2));
-      this.position.y -= Math.round((TILE * speed / FRAMERATE) / Math.sqrt(2));
+      this.position.x -= ((TILE * speed / FRAMERATE) / Math.sqrt(2));
+      this.position.y -= ((TILE * speed / FRAMERATE) / Math.sqrt(2));
       this.hitbox = new Rectangle(position.x + 5, position.y + 5, 40, 40);
       lastMovedDirection = direction;
       previousDirection = direction;
@@ -161,8 +226,8 @@ public class Zombie extends Character
     {
 
       // free movement
-      this.position.x -= Math.round((TILE * speed / FRAMERATE) / Math.sqrt(2));
-      this.position.y += Math.round((TILE * speed / FRAMERATE) / Math.sqrt(2));
+      this.position.x -= ((TILE * speed / FRAMERATE) / Math.sqrt(2));
+      this.position.y += ((TILE * speed / FRAMERATE) / Math.sqrt(2));
       this.hitbox = new Rectangle(position.x + 5, position.y + 5, 40, 40);
       lastMovedDirection = direction;
       previousDirection = direction;
@@ -173,8 +238,8 @@ public class Zombie extends Character
     {
 
       // free movement
-      this.position.x += Math.round((TILE * speed / FRAMERATE) / Math.sqrt(2));
-      this.position.y -= Math.round((TILE * speed / FRAMERATE) / Math.sqrt(2));
+      this.position.x += ((TILE * speed / FRAMERATE) / Math.sqrt(2));
+      this.position.y -= ((TILE * speed / FRAMERATE) / Math.sqrt(2));
       this.hitbox = new Rectangle(position.x + 5, position.y + 5, 40, 40);
       lastMovedDirection = direction;
       previousDirection = direction;
@@ -185,8 +250,8 @@ public class Zombie extends Character
     {
 
       // free movement
-      this.position.x += Math.round((TILE * speed / FRAMERATE) / Math.sqrt(2));
-      this.position.y += Math.round((TILE * speed / FRAMERATE) / Math.sqrt(2));
+      this.position.x += ((TILE * speed / FRAMERATE) / Math.sqrt(2));
+      this.position.y += ((TILE * speed / FRAMERATE) / Math.sqrt(2));
       this.hitbox = new Rectangle(position.x + 5, position.y + 5, 40, 40);
       lastMovedDirection = direction;
       previousDirection = direction;
